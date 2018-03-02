c82155bec1a753733adc456dac2eb5f007a5b624
Hannes <>
2018-03-02 15:22:36 +0100

    removed models

--- server/server.py
+++ server/server.py
@@ -14,9 +14,7 @@ app.logger.addHandler(logging.StreamHandler())
 app.logger.setLevel(logging.INFO)
 app_api = Api(app)
 app_api.add_resource(api.SwaggerAPI, '/swagger')
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{user}:{password}@{host}/{db}".format(user=os.environ["POSTGRES_USER"], password=os.environ["POSTGRES_PASSWORD"], host=os.environ["POSTGRES_HOST"], db=os.environ["POSTGRES_DB"])
 db.app_db.init_app(app)
-migrate = Migrate(app, db.app_db)
 
 @app.after_request
 def after_request(response):
