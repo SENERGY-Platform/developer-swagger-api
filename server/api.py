b5810ecd7573c98d8d5737c107a94e0ecd70c465
Hannes <>
2018-03-02 15:53:26 +0100

    update mongodb insert

--- server/api.py
+++ server/api.py
@@ -12,7 +12,9 @@ from prance import ResolvingParser
 class SwaggerAPI(Resource):
     def get(self):
         response = requests.get("http://petstore.swagger.io/v2/swagger.json").json()
-        db.db.swagger.insert(response)
+        db.db["swagger"].insert({
+            "/path": response
+        })
         cursor = db.db.find({})
         for document in cursor:
             print(document)
