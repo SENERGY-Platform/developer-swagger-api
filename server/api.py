9948b49a5bbd301ea265f50ce2506beba34638fe
Hannes <mail@hannesh.de>
2018-04-04 12:49:53 +0200

    changed logging

--- server/api.py
+++ server/api.py
@@ -30,9 +30,9 @@ class PublicSwaggerAPI(Resource):
         for swagger in all_swagger:
             # json load, otherwise the json string gets escaped with jsonify
             complete_swagger = json.loads(swagger.get("swagger"))
+            server.app.logger.info(public_apis)
+
             for api in public_apis:
-                server.app.logger.info(complete_swagger)
-                server.app.logger.info(api)
                 if complete_swagger.get("basePath") == api.get("uris")[0]:
                     all_swagger_with_permission.append(complete_swagger)
         return jsonify(all_swagger_with_permission)
