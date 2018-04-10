39e867da69f7ba9f3379ccc634d5f976c1215070
Hannes Hansen <Hansi@Hanness-MBP.fritz.box>
2018-04-10 14:23:32 +0200

    ficex swagger generation for non admins

--- server/api.py
+++ server/api.py
@@ -12,6 +12,35 @@ import json
 import copy
 import jwt
 
+def transform_swagger_permission(swagger):
+    filtered_swagger = copy.deepcopy(swagger)
+    # Remove Methods and path were the role has no permissions
+    for path in swagger.get("paths"):
+        if path:
+            for method in swagger.get("paths")[path]:
+                if method:
+                    user_has_permission = False
+                    for role in roles:
+                        transformed_path = (swagger.get("basePath") + path).replace("/", ":")
+                        payload = {
+                            "subject": role,
+                            "action": method.upper(),
+                            "resource":  "endpoints" + transformed_path
+                        }
+                        ladon = "{url}/access".format(url=os.environ["LADON_URL"])
+                        response = requests.get(ladon, data=json.dumps(payload)).json()
+                        server.app.logger.info("check for authorization at ladon: ")
+                        server.app.logger.info("Request Data: " + json.dumps(payload))
+                        server.app.logger.info("Response Data: " + json.dumps(response))
+                        if response.get("Result"):
+                            user_has_permission = True
+                            break
+                                    
+                    if not user_has_permission:
+                        del filtered_swagger.get("paths")[path][method]
+    return filtered_swagger
+            
+
 class SwaggerAPI(Resource):
     def get(self):
         token = jwt.decode(request.headers.get("Authorization").split(" ")[1], verify=False)
@@ -24,42 +53,19 @@ class SwaggerAPI(Resource):
         if "admin" in roles:
             return jsonify([json.loads(swagger.get("swagger")) for swagger in all_swagger])
         else:
+            filtered_swagger = []
             for swagger in all_swagger:
                 # json load, otherwise the json string gets escaped with jsonify
                 complete_swagger = json.loads(swagger.get("swagger"))
-                filtered_swagger = copy.deepcopy(complete_swagger)
                 # Check if API is public accessible
                 if complete_swagger.get("host") == "api.sepl.infai.org": 
                     for api in public_apis:
                         if complete_swagger.get("basePath") == api.get("uris")[0]:
-                            all_swagger_with_permission.append(complete_swagger)
+                            transformed_swagger = transform_swagger_permission(complete_swagger)
+                            filtered_swagger.append(transformed_swagger)
                 else:
                     # APIs is not in KONG, therefor accessible
-                    all_swagger_with_permission.append(complete_swagger)
+                    filtered_swagger.append(complete_swagger)
 
-                # Remove Methods and path were the role has no permissions
-                for path in complete_swagger.get("paths"):
-                    if path:
-                        for method in complete_swagger.get("paths")[path]:
-                            if method:
-                                user_has_permission = False
-                                for role in roles:
-                                    transformed_path = (complete_swagger.get("basePath") + path).replace("/", ":")
-                                    payload = {
-                                        "subject": role,
-                                        "action": method.upper(),
-                                        "resource":  "endpoints" + transformed_path
-                                    }
-                                    ladon = "{url}/access".format(url=os.environ["LADON_URL"])
-                                    response = requests.get(ladon, data=json.dumps(payload)).json()
-                                    server.app.logger.info("check for authorization at ladon: ")
-                                    server.app.logger.info("Request Data: " + json.dumps(payload))
-                                    server.app.logger.info("Response Data: " + json.dumps(response))
-                                    if response.get("Result"):
-                                        user_has_permission = True
-                                        break
-                                    
-                                if not user_has_permission:
-                                    del filtered_swagger.get("paths")[path][method]
-            return jsonify(filtered_swagger)
+                return jsonify(filtered_swagger)
 
