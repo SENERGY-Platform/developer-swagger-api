f259f2c35f2e20165ece156dcbf8d686885dc9c4
Hannes Hansen <Hansi@Hanness-MBP.fritz.box>
2018-05-30 10:46:25 +0200

    check if document is json before loading into database

--- server/api.py
+++ server/api.py
@@ -51,8 +51,11 @@ class SwaggerAPI(Resource):
         all_swagger = db.db["swagger"].find({})
         public_apis = server.getApisFromKong()
         if "admin" in roles:
+            app.logger.info("user role is admin -> return all")
             return jsonify([json.loads(swagger.get("swagger")) for swagger in all_swagger])
         else:
+            app.logger.info("user role is not admin -> remove paths from swagger where user role does not have access")
+            filtered_swagger = []
             for swagger in all_swagger:
                 # json load, otherwise the json string gets escaped with jsonify
                 complete_swagger = None
@@ -62,13 +65,10 @@ class SwaggerAPI(Resource):
                     server.app.logger.info(e)
 
                 if complete_swagger:
-                    filtered_swagger = []
-                    server.app.logger.info(complete_swagger)
+                    app.logger.info("swagger file was parsed to json")
                     # Check if API is public accessible
                     if complete_swagger.get("host") == "api.sepl.infai.org": 
                         for api in public_apis:
-                            server.app.logger.info(complete_swagger.get("basePath"))
-                            server.app.logger.info(api.get("uris")[0])
                             if complete_swagger.get("basePath") == api.get("uris")[0]:
                                 transformed_swagger = transform_swagger_permission(complete_swagger, roles)
                                 filtered_swagger.append(transformed_swagger)
