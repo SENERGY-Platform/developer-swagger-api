5b5e1c882351ec67aca05030afdd898dc493bc7f
franzmueller <franzmueller@users.noreply.github.com>
2020-07-03 14:19:56 +0200

    SNRGY-797 fix not inserting new doc files

--- apis/db/db.py
+++ apis/db/db.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
 import os
+
+from pymongo.collection import Collection
+from pymongo.database import Database
+
 import apis.util.kong as kong
 import requests
 from threading import Timer
@@ -12,7 +16,8 @@ if "DB_HOST" not in os.environ or "DB_HOST" == "" or "DB_PORT" not in os.environ
     logger.error("DB_HOST and/or DB_PORT not set")
 else:
     client = MongoClient('mongodb://{host}:{port}/'.format(host=os.environ["DB_HOST"], port=os.environ["DB_PORT"]))
-    db = client.swagger
+    db = Database(client, "swagger")
+    collection = Collection(db, "swagger")
 
 
 def load_doc():
@@ -23,7 +28,8 @@ def load_doc():
     for route in kong_routes:
         try:
             logging.info(json.dumps(route))
-            logging.info("Trying to get doc from " + str(route.get("paths")[0]))
+            path = str(route.get("paths")[0])
+            logging.info("Trying to get doc from " + path)
             upstream_service = kong.get_upstream(kong_services, route.get("service").get("id"))
             upstream_url = upstream_service.get("protocol") + "://" + upstream_service.get("host") + ":" + str(
                 upstream_service.get("port")) + "/doc"
@@ -31,11 +37,12 @@ def load_doc():
             response = requests.get(upstream_url, timeout=1)
             logging.info("Got response with code " + str(response.status_code))
             if response.status_code == 200:
+                logging.debug("Length is " + str(len(response.text)))
                 try:
                     # if entry contains empty basepath, replace with basePath from Kong
                     logging.info("Setting basePath if not set")
                     definition = response.text.replace("\"basePath\": \"/\"",
-                                                       "\"basePath\": \"" + str(route.get("paths")[0]) + "/\"")
+                                                       "\"basePath\": \"" + path + "/\"")
                     if definition.find('host') == -1:
                         logging.info("Did not find host entry, setting to " + os.environ['KONG_HOST'])
                         # if entry contains no host, set KONG_HOST
@@ -47,16 +54,11 @@ def load_doc():
                         # if entry containes no schemes, add http/https
                         definition = definition.replace("\"swagger\": \"2.0\",",
                                                         "\"swagger\": \"2.0\",\n\"schemes\": [\"https\", \"http\"],")
-                    json.loads(definition)
-                    db.db["swagger"].remove({})
-                    db.db["swagger"].insert({
-                        "swagger": definition
-                    })
+                    collection.replace_one({"path": path}, {"path": path, "swagger": definition}, upsert=True)
                     logging.info(
-                        "inserted swagger file from documentation endpoint of service " + str(route.get("paths")[0]))
+                        "inserted swagger file from documentation endpoint of service " + path)
                 except ValueError as e:
-                    logging.info("document from" + str(
-                        route.get("paths")[0]) + " /doc endpoint is not json, therefore dont get loaded")
+                    logging.info(str(e))
             else:
                 logging.info(str(route.get("paths")[0]) + " responded with " + str(
                     response.status_code) + ". No documentation added.")
@@ -67,4 +69,4 @@ def load_doc():
 
 
 def get_swagger_files():
-    return db["swagger"].find({})
+    return collection.find({})
